65a5304935251ba88d2d8b38457aa8243dbe9acb
Celso Rodrigues <celso.rodrigues.dev@gmail.com>
2021-03-12 20:45:12 +0000

    before targeting cards

---
+++ Client/Assets/Scripts/Visual/MessageManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class MessageManager : MonoBehaviour 
+{
+    public Text messageText;
+    public GameObject messagePanel;
+
+    public static MessageManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        messagePanel.SetActive(false);
+    }
+
+    public void ShowMessage(string message, float duration, Command com) => StartCoroutine(ShowMessageCoroutine(message, duration, com));
+
+    IEnumerator ShowMessageCoroutine(string message, float duration, Command com)
+    {
+        messageText.text = message;
+        messagePanel.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        messagePanel.SetActive(false);
+        // TODO Command.CommandExecutionComplete();
+    }
+    
+}
